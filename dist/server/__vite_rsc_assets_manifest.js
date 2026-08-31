1e47d355a92ff0bc9081f1c3f9ffc658c637ac25
Bojimmy <128234791+Bojimmy@users.noreply.github.com>
2026-08-31 19:32:08 -0400

    feat: expose governed Foundry tools through MCP

--- dist/server/__vite_rsc_assets_manifest.js
+++ dist/server/__vite_rsc_assets_manifest.js
@@ -1,76 +1,76 @@
 export default {
-  "clientEntryUrl": "/_next/static/chunks/index-DWx1eXSg.js",
+  "clientEntryUrl": "/_next/static/chunks/index-WqydU5nd.js",
   "clientEntryDeps": {
     "js": [
-      "/_next/static/chunks/index-DWx1eXSg.js",
+      "/_next/static/chunks/index-WqydU5nd.js",
       "/_next/static/chunks/rolldown-runtime-hePW80VL.js",
       "/_next/static/chunks/framework-DTZGTDtF.js",
-      "/_next/static/chunks/vinext-BsuFoI2h.js"
+      "/_next/static/chunks/vinext-JYcXM_9x.js"
     ],
     "css": []
   },
   "clientReferenceDeps": {
     "e9dd5bc99e42": {
       "js": [
-        "/_next/static/chunks/vinext-BsuFoI2h.js",
+        "/_next/static/chunks/vinext-JYcXM_9x.js",
         "/_next/static/chunks/rolldown-runtime-hePW80VL.js",
         "/_next/static/chunks/framework-DTZGTDtF.js",
-        "/_next/static/chunks/index-DWx1eXSg.js"
+        "/_next/static/chunks/index-WqydU5nd.js"
       ],
       "css": []
     },
     "b77bc4909da7": {
       "js": [
-        "/_next/static/chunks/vinext-BsuFoI2h.js",
+        "/_next/static/chunks/vinext-JYcXM_9x.js",
         "/_next/static/chunks/rolldown-runtime-hePW80VL.js",
         "/_next/static/chunks/framework-DTZGTDtF.js",
-        "/_next/static/chunks/index-DWx1eXSg.js"
+        "/_next/static/chunks/index-WqydU5nd.js"
       ],
       "css": []
     },
     "e43174d2e269": {
       "js": [
-        "/_next/static/chunks/vinext-BsuFoI2h.js",
+        "/_next/static/chunks/vinext-JYcXM_9x.js",
         "/_next/static/chunks/rolldown-runtime-hePW80VL.js",
         "/_next/static/chunks/framework-DTZGTDtF.js",
-        "/_next/static/chunks/index-DWx1eXSg.js"
+        "/_next/static/chunks/index-WqydU5nd.js"
       ],
       "css": []
     },
     "7ab746c10cf6": {
       "js": [
-        "/_next/static/chunks/vinext-BsuFoI2h.js",
+        "/_next/static/chunks/vinext-JYcXM_9x.js",
         "/_next/static/chunks/rolldown-runtime-hePW80VL.js",
         "/_next/static/chunks/framework-DTZGTDtF.js",
-        "/_next/static/chunks/index-DWx1eXSg.js"
+        "/_next/static/chunks/index-WqydU5nd.js"
       ],
       "css": []
     },
     "5b59d0314280": {
       "js": [
-        "/_next/static/chunks/layout-segment-context-BoN07BKl.js",
+        "/_next/static/chunks/layout-segment-context-BgALjSNW.js",
         "/_next/static/chunks/rolldown-runtime-hePW80VL.js",
         "/_next/static/chunks/framework-DTZGTDtF.js",
-        "/_next/static/chunks/vinext-BsuFoI2h.js",
-        "/_next/static/chunks/index-DWx1eXSg.js"
+        "/_next/static/chunks/vinext-JYcXM_9x.js",
+        "/_next/static/chunks/index-WqydU5nd.js"
       ],
       "css": []
     },
     "7fb186dce000": {
       "js": [
-        "/_next/static/chunks/vinext-BsuFoI2h.js",
+        "/_next/static/chunks/vinext-JYcXM_9x.js",
         "/_next/static/chunks/rolldown-runtime-hePW80VL.js",
         "/_next/static/chunks/framework-DTZGTDtF.js",
-        "/_next/static/chunks/index-DWx1eXSg.js"
+        "/_next/static/chunks/index-WqydU5nd.js"
       ],
       "css": []
     },
     "e6ebc35dea2e": {
       "js": [
-        "/_next/static/chunks/vinext-BsuFoI2h.js",
+        "/_next/static/chunks/vinext-JYcXM_9x.js",
         "/_next/static/chunks/rolldown-runtime-hePW80VL.js",
         "/_next/static/chunks/framework-DTZGTDtF.js",
-        "/_next/static/chunks/index-DWx1eXSg.js"
+        "/_next/static/chunks/index-WqydU5nd.js"
       ],
       "css": []
     }
